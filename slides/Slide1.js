b01a2ad3ef6bf1165a7a613ebf9a7d14c60a9e6b
uday2305 <udaybhaskar2305@gmail.com>
2016-11-26 22:21:51 +0100

    Presentation on React made with React.

---
+++ slides/Slide1.js
@@ -0,0 +1,78 @@
+class Slide1 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+        <div>
+              <h1>Current Slide One</h1>
+        </div>
+    );
+  }
+}
+
+class Slide2 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+        <div>
+              <h1>Current Slide two</h1>
+        </div>
+    );
+  }
+}
+
+class Slide3 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+        <div>
+              <h1>Current Slide three</h1>
+        </div>
+    );
+  }
+}
+
+class Slide4 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+        <div>
+              <h1>Current Slide Four</h1>
+        </div>
+    );
+  }
+}
+
+class Slide5 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+        <div>
+              <h1>Current Slide Five</h1>
+        </div>
+    );
+  }
+}
+
+class Slide6 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render(){
+    return(
+        <div>
+              <h1>Current Slide Six</h1>
+        </div>
+    );
+  }
+}
+export { Slide1 as Slide1,Slide2 as Slide2,Slide3 as Slide3,Slide4 as Slide4,Slide5 as Slide5,Slide6 as Slide6 } 
